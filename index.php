c2a85eec041c4637741df3726f68f8afdbbe27f5
MarinaRumy <marinadevi120@gmail.com>
2022-03-19 09:29:35 +0900

    add every changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+//Loop
+
+//While Loop
+//Do While Loop
+//For Loop
+//Foreach Loop
+$array = array("Daniel", "Jane", "Jacob", "John", "Mariane");
+
+foreach($array as $loopdata){
+    echo"My name is ".$loopdata."<br>";
+}
+?>
+</body>
+</html>
